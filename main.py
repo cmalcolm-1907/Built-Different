07629a078eb4080a9c7fabf92e8a150ed9950b82
cmalcolm <cjmalc@gmail.com>
2025-01-22 18:24:26 +1300

    Added some basic JSON placeholder databases

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
-print("HELLO WORLD!")
+# A Mech themed football manager style game
+
+# Pilots
+Pilots={
+    'Alice':{
+        'skill':10,
+        'mech':'coyote'
+        'salary'10
+    },
+    'Brent':{
+        'skill':7,
+        'mech':'anteater',
+        'salary':6
+    },
+    'Connor':{
+         'skill':7,
+         'mech':'badger',
+         'salary':6
+    },
+     'David':{
+        'skill':7,
+        'mech':'coyote',
+        'salary':6
+    },
+}
+
+Mechs={
+    'anteater':{
+        'durability':7,
+        'power':4,
+        'speed':3,
+    },
+    'badger':{
+        'durability':7,
+        'power':5,
+        'speed':5
+    },
+    'coyote':{
+        'durability':5,
+        'speed':6,
+        'power':8,
+        'cost':50
+    }
+}
+Teams={
+    'Alpha Team':{
+        pilots=['Alice','Connor'],
+        wins=0,
+        losses=0,
+        funds=200
+    },
+    'Bravo Team':{
+        pilots=['Brent','David'],
+        winse=0,
+        losses=0,
+        funds=200
+    }
+}
