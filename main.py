6d9749341e1ac0053e20943f1a3b89bedd2a7ced
cmalcolm <cjmalc@gmail.com>
2025-01-23 10:45:27 +1300

    fixed up some class work

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ class Pilot:
         self.salary=salary
 
     def __str__(self):
-        return f'{self.name}\nSkill: {self.skill}\nMech: {self.mech}\nSalary: {self.salary}\n'
+        returnstring = f'{self.name}\nSkill: {self.skill}\n'
+        returnstring += str(self.mech)
+        returnstring += f'\nSalary: {self.salary}\n'
+        return returnstring
 
 class Mech:
     def __init__(self,name,durability,power,speed,cost):
@@ -21,6 +24,21 @@ class Mech:
         
     def __str__(self):
         return f'{self.name}\nDurability: {self.durability}\nPower: {self.power}\nSpeed: {self.speed}\nCost: ${self.cost}'
+
+class Team:
+    def __init__(self,name,pilots,wins,losses,funds):
+        self.name=name
+        self.pilots=pilots
+        self.wins=wins
+        self.losses=losses
+        self.funds=funds       
+
+    def __str__(self):
+        returnstring=f'{self.name}\n\nWins: {self.wins}\nLosses: {self.losses}\nFunds: {self.funds}\n\nPilots\n'
+        for pilot in self.pilots:
+            returnstring+=str(pilot)+'\n'
+        return returnstring
+        
     
 Pilots={
     'Alice':Pilot('Alice',8,'coyote',80),
@@ -30,42 +48,20 @@ Pilots={
 }
 
 Mechs={
-    'anteater':{
-        'durability':7,
-        'power':4,
-        'speed':3,
-    },
-    'badger':{
-        'durability':7,
-        'power':5,
-        'speed':5
-    },
-    'coyote':{
-        'durability':5,
-        'speed':6,
-        'power':8,
-        'cost':50
-    }
+    'anteater':Mech('anteater',7,4,3,140),
+    'badger':Mech('badger',6,5,5,160),
+    'coyote':Mech('coyote',5,6,7,180),
 }
 Teams={
-    'Alpha Team':{
-        'pilots':['Alice','Connor'],
-        'wins':0,
-        'losses':0,
-        'funds':200
-    },
-    'Bravo Team':{
-        'pilots':['Brent','David'],
-        'winse':0,
-        'losses':0,
-        'funds':200
-    }
+    'Alpha Team':Team('Alpha Team',[Pilots['Alice'],Pilots['Connor']],0,0,200),
+    'Bravo Team':Team('Bravo Team',[Pilots['David'],Pilots['Brent']],0,0,200),
 }
 
 def combat(t1,t2):
-    pass
+    return t1,t2
 
 def mainloop():
-    pass
+    for team in Teams:
+        print(Teams[team])
 
 mainloop()
