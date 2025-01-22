7db37af2da0f33e893d09291c4c7085a2b135df0
cmalcolm <cjmalc@gmail.com>
2025-01-23 08:48:25 +1300

    changed pilots from a dict of dicts, to a dict of classes

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 # A Mech themed football manager style game
 
 # Pilots
+class Pilot:
+    def __init__(self,name,skill,mech,salary):
+        self.name=name
+        self.skill=skill
+        self.mech=mech
+        self.salary=salary
+
+    def __str__(self):
+        return f'{self.name}\nSkill: {self.skill}\nMech: {self.mech}\nSalary: {self.salary}\n'
+
+class Mech:
+    def __init__(self,name,durability,power,speed,cost):
+        self.name=name
+        self.durability=durability
+        self.power=power,
+        self.speed=speed
+        self.cost=cost
+        
+    def __str__(self):
+        return f'{self.name}\nDurability: {self.durability}\nPower: {self.power}\nSpeed: {self.speed}\nCost: ${self.cost}'
+    
 Pilots={
-    'Alice':{
-        'skill':10,
-        'mech':'coyote'
-        'salary'10
-    },
-    'Brent':{
-        'skill':7,
-        'mech':'anteater',
-        'salary':6
-    },
-    'Connor':{
-         'skill':7,
-         'mech':'badger',
-         'salary':6
-    },
-     'David':{
-        'skill':7,
-        'mech':'coyote',
-        'salary':6
-    },
+    'Alice':Pilot('Alice',8,'coyote',80),
+    'Brent':Pilot('Brent',7,'anteater',65),
+    'Connor':Pilot('Connor',6,'badger',60),
+    'David':Pilot('David',7,'coyote',70),
 }
 
 Mechs={
@@ -44,15 +49,23 @@ Mechs={
 }
 Teams={
     'Alpha Team':{
-        pilots=['Alice','Connor'],
-        wins=0,
-        losses=0,
-        funds=200
+        'pilots':['Alice','Connor'],
+        'wins':0,
+        'losses':0,
+        'funds':200
     },
     'Bravo Team':{
-        pilots=['Brent','David'],
-        winse=0,
-        losses=0,
-        funds=200
+        'pilots':['Brent','David'],
+        'winse':0,
+        'losses':0,
+        'funds':200
     }
 }
+
+def combat(t1,t2):
+    pass
+
+def mainloop():
+    pass
+
+mainloop()
