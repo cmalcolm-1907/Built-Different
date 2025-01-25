a50e1a6894e57ecc6831d48948a00e7afbbe55a8
cmalcolm <cjmalc@gmail.com>
2025-01-26 08:44:08 +1300

    began working with PyQt5 for a gui

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 # A Mech themed football manager style game
+# Modules
 import sqlite3
+from PyQt5.QtCore import QSize, Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+import sys
 
-con = sqlite3.connect('builtdifferent.db')
+# Constants and Variables
+# datbase connection and cursor
+con = sqlite3.connect("builtdifferent.db")
 cur = con.cursor()
 
 
+# Custom Classes
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
+        self.setWindowTitle("Built Different")
+        button = QPushButton("Push Me")
+
+        self.setCentralWidget(button)
+
+
+# Custom Methods/Functions
 def view_team(name):
     for row in cur.execute(f"SELECT * FROM Teams WHERE name IS '{name}'"):
         print(row)
 
-view_team('Blitz Team')
+
+# GUI
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+# Mainloop
+app.exec()
