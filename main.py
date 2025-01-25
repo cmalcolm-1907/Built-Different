c953d1ed65650d384c18b696c310fd2738d0ee6b
cmalcolm <cjmalc@gmail.com>
2025-01-26 10:56:04 +1300

    trying combo boxes

--- main.py
+++ main.py
@@ -2,7 +2,14 @@
 # Modules
 import sqlite3
 from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QMainWindow,
+    QVBoxLayout,
+    QComboBox,
+    QPushButton,
+)
 import sys
 
 # Constants and Variables
@@ -17,9 +24,33 @@ class MainWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("Built Different")
-        button = QPushButton("Push Me")
+        combobox1 = QComboBox()
+        combobox1.addItem("One")
+        combobox1.addItem("Two")
+        combobox1.addItem("Three")
+        combobox1.addItem("Four")
 
-        self.setCentralWidget(button)
+        combobox2 = QComboBox()
+        combobox2.addItems(["One", "Two", "Three", "Four"])
+
+        combobox3 = QComboBox()
+        combobox3.addItems(["One", "Two", "Three", "Four"])
+        combobox3.insertItem(2, "Hello!")
+
+        combobox4 = QComboBox()
+        combobox4.addItems(["One", "Two", "Three", "Four"])
+        combobox4.insertItems(2, ["Hello!", "again"])
+
+        layout = QVBoxLayout()
+        layout.addWidget(combobox1)
+        layout.addWidget(combobox2)
+        layout.addWidget(combobox3)
+        layout.addWidget(combobox4)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setCentralWidget(container)
 
 
 # Custom Methods/Functions
@@ -30,9 +61,8 @@ def view_team(name):
 
 # GUI
 app = QApplication(sys.argv)
-
-window = MainWindow()
-window.show()
+w = MainWindow()
+w.show()
 
 # Mainloop
 app.exec()
